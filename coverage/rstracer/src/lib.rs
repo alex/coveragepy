b66008a5ff6238df49d01c146be0ff9bf02dd98a
Alex Gaynor <alex.gaynor@gmail.com>
2022-05-29 18:07:24 -0400

    Random progress

--- coverage/rstracer/src/lib.rs
+++ coverage/rstracer/src/lib.rs
@@ -1,9 +1,13 @@
-use std::cell::Cell;
+use pyo3::types::{PyBytes, PyCode, PyDict, PyFrame, PyModule, PySet, PyString};
 use pyo3::AsPyPointer;
 use pyo3::IntoPy;
 use std::collections::HashSet;
 use std::os::raw::c_int;
 use std::ptr;
+
+// TODO: These change by Python version!
+const YIELD_VALUE: u8 = 86;
+const YIELD_FROM: u8 = 72;
 
 #[pyo3::prelude::pyclass]
 struct CTracer {
@@ -16,9 +20,14 @@ struct CTracer {
     #[pyo3(get, set)]
     warn: Option<pyo3::PyObject>,
 
+    #[pyo3(get, set)]
+    should_trace_cache: pyo3::Py<PyDict>,
+
+    #[pyo3(get, set)]
+    trace_arcs: bool,
+
     original_data: pyo3::Py<pyo3::PyAny>,
 
-    should_trace_cache: pyo3::Py<pyo3::types::PyDict>,
     data_stack: Vec<DataStackEntry>,
 }
 
@@ -37,10 +46,11 @@ impl CTracer {
             activity: false,
             should_trace: None,
             warn: None,
+            trace_arcs: false,
 
             original_data: py.None(),
 
-            should_trace_cache: pyo3::types::PyDict::new(py).into_py(py),
+            should_trace_cache: PyDict::new(py).into_py(py),
             data_stack: vec![],
         }
     }
@@ -48,18 +58,6 @@ impl CTracer {
     #[setter]
     fn data(&mut self, value: pyo3::Py<pyo3::PyAny>) {
         self.original_data = value;
-    }
-
-    #[setter]
-    fn trace_arcs(&mut self, value: &pyo3::PyAny) {
-        // XXX
-        assert!(!value.is_true().unwrap());
-    }
-
-    #[setter]
-    fn should_trace_cache(&mut self, value: &pyo3::PyAny) {
-        // XXX
-        assert!(!value.is_true().unwrap());
     }
 
     fn get_stats(&self) {}
@@ -87,11 +85,8 @@ impl CTracer {
         // XXX: is this the right place for this?
         let original_data = self.original_data.as_ref(py);
         for (filename, disposition) in self.should_trace_cache.as_ref(py).iter() {
-            let set: &pyo3::types::PySet = original_data
-                .call_method1(
-                    "setdefault",
-                    (filename, pyo3::types::PySet::empty(py).unwrap()),
-                )
+            let set: &PySet = original_data
+                .call_method1("setdefault", (filename, PySet::empty(py).unwrap()))
                 .unwrap()
                 .extract()
                 .unwrap();
@@ -119,15 +114,15 @@ impl CTracer {
 
         match event {
             pyo3::ffi::PyTrace_CALL => self.handle_call(py, frame),
-            pyo3::ffi::PyTrace_RETURN => self.handle_return(frame),
+            pyo3::ffi::PyTrace_RETURN => self.handle_return(py, frame),
             pyo3::ffi::PyTrace_LINE => self.handle_line(py, frame),
             _ => Ok(()),
         }
     }
 
     fn handle_call(&mut self, py: pyo3::Python<'_>, frame: &PyFrame) -> pyo3::PyResult<()> {
-        let code = frame.get_code();
-        let filename: &pyo3::PyAny = unsafe { py.from_borrowed_ptr((*code).co_filename) };
+        let code = frame_get_code(py, frame);
+        let filename = code_get_filename(py, code);
 
         let disposition = match self.should_trace_cache.as_ref(py).get_item(filename) {
             Some(value) => value,
@@ -143,11 +138,11 @@ impl CTracer {
             }
         };
 
-        let real_call = frame.get_lasti() < 0;
+        let real_call = frame_get_lasti(frame) < 0;
         let last_line = if real_call {
-            unsafe { -(*code).co_firstlineno }
+            -code_get_firstlineno(code)
         } else {
-            frame.get_line_number()
+            frame_get_line_number(frame)
         };
 
         if !disposition.is_none() {
@@ -168,24 +163,53 @@ impl CTracer {
         Ok(())
     }
 
-    fn handle_return(&mut self, _frame: &PyFrame) -> pyo3::PyResult<()> {
-        if let Some(_entry) = self.data_stack.pop() {}
+    fn handle_return(&mut self, py: pyo3::Python<'_>, frame: &PyFrame) -> pyo3::PyResult<()> {
+        if let Some(entry) = self.data_stack.pop() {
+            if let Some(disposition) = entry.disposition {
+                if self.trace_arcs {
+                    let code_obj = frame_get_code(py, frame);
+                    let code = code_get_code(py, code_obj);
+                    let lasti = frame_get_lasti(frame) as usize;
+
+                    let mut is_yield = false;
+                    let mut is_yield_from = false;
+                    if lasti < code.len() {
+                        is_yield = code[lasti] == YIELD_VALUE;
+                        if lasti + 2 < code.len() {
+                            is_yield_from = code[lasti + 2] == YIELD_FROM;
+                        }
+                    }
+                    if !(is_yield || is_yield_from) {
+                        let pair = pack_pair(entry.last_line, -code_get_firstlineno(code_obj));
+                        disposition.as_ref(py).borrow_mut().file_data.insert(pair);
+                    }
+                }
+            }
+        }
         Ok(())
     }
 
     fn handle_line(&mut self, py: pyo3::Python<'_>, frame: &PyFrame) -> pyo3::PyResult<()> {
         if let Some(mut entry) = self.data_stack.last_mut() {
-            // if entry.file_tracer
+            let mut disposition = if let Some(ref d) = entry.disposition {
+                d.as_ref(py).borrow_mut()
+            } else {
+                return Ok(());
+            };
+
             let (lineno_from, lineno_to) = {
-                let l = frame.get_line_number();
+                let l = frame_get_line_number(frame);
                 (l, l)
             };
 
-            let mut disposition = entry.disposition.as_ref().unwrap().as_ref(py).borrow_mut();
-
             if lineno_from != -1 {
                 for lineno in lineno_from..=lineno_to {
-                    disposition.file_data.insert(lineno);
+                    if self.trace_arcs {
+                        let value = pack_pair(entry.last_line, lineno);
+                        disposition.file_data.insert(value);
+                    } else {
+                        disposition.file_data.insert(lineno.into());
+                    }
                     entry.last_line = lineno;
                 }
             }
@@ -195,32 +219,54 @@ impl CTracer {
     }
 }
 
-struct PyFrame(*mut pyo3::ffi::PyFrameObject);
+fn pack_pair(v1: i32, v2: i32) -> i64 {
+    let mut packed = 0i64;
+    let v1 = if v1 < 0 {
+        packed |= 1 << 40;
+        -v1 as i64
+    } else {
+        v1 as i64
+    };
+    let v2 = if v2 < 0 {
+        packed |= 1 << 41;
+        -v2 as i64
+    } else {
+        v2 as i64
+    };
+    packed | (v2 << 20) | v1
+}
 
-impl pyo3::IntoPy<pyo3::Py<pyo3::PyAny>> for &PyFrame {
-    fn into_py(self, py: pyo3::Python<'_>) -> pyo3::Py<pyo3::PyAny> {
-        unsafe {
-            py.from_borrowed_ptr::<pyo3::PyAny>(self.0.cast::<pyo3::ffi::PyObject>())
-                .into_py(py)
-        }
+fn frame_get_line_number(frame: &PyFrame) -> i32 {
+    unsafe { pyo3::ffi::PyFrame_GetLineNumber(frame.as_ptr().cast::<pyo3::ffi::PyFrameObject>()) }
+}
+
+fn frame_get_code<'p>(py: pyo3::Python<'p>, frame: &'p PyFrame) -> &'p PyCode {
+    unsafe {
+        #[cfg(Py_3_9)]
+        let code = pyo3::ffi::PyFrame_GetCode(frame.as_ptr());
+        #[cfg(not(Py_3_9))]
+        let code = (*frame.as_ptr().cast::<pyo3::ffi::PyFrameObject>()).f_code;
+
+        py.from_borrowed_ptr(code.cast())
     }
 }
 
-impl PyFrame {
-    fn get_line_number(&self) -> i32 {
-        unsafe { pyo3::ffi::PyFrame_GetLineNumber(self.0) }
-    }
+fn frame_get_lasti(frame: &PyFrame) -> i32 {
+    unsafe { (*frame.as_ptr().cast::<pyo3::ffi::PyFrameObject>()).f_lasti }
+}
 
-    fn get_lasti(&self) -> i32 {
-        unsafe { (*self.0).f_lasti }
-    }
+fn code_get_filename<'p>(py: pyo3::Python<'p>, code: &'p PyCode) -> &'p pyo3::PyAny {
+    unsafe { py.from_borrowed_ptr((*code.as_ptr().cast::<pyo3::ffi::PyCodeObject>()).co_filename) }
+}
 
-    fn get_code(&self) -> *mut pyo3::ffi::PyCodeObject {
-        #[cfg(Py_3_9)]
-        unsafe { pyo3::ffi::PyFrame_GetCode(self.0) }
-        #[cfg(not(Py_3_9))]
-        unsafe { (*self.0).f_code }
-    }
+fn code_get_firstlineno(code: &PyCode) -> i32 {
+    unsafe { (*code.as_ptr().cast::<pyo3::ffi::PyCodeObject>()).co_firstlineno }
+}
+
+fn code_get_code<'p>(py: pyo3::Python<'p>, code: &'p PyCode) -> &'p [u8] {
+    let py_code_bytes: &PyBytes =
+        unsafe { py.from_owned_ptr((*code.as_ptr().cast::<pyo3::ffi::PyCodeObject>()).co_code) };
+    py_code_bytes.as_bytes()
 }
 
 // This ought to be an `unsafe fn` but `PyEval_SetTrace` takes a regular `fn`
@@ -231,20 +277,10 @@ extern "C" fn trace_func(
     event: c_int,
     _arg: *mut pyo3::ffi::PyObject,
 ) -> c_int {
-    // Calling `acquire_gil` ocassionally is necessary to flush pyo3's
-    // `register_owned` stack. Calling it on every invocation has a huge
-    // performance penalty. So we call it sometimes.
-    thread_local! {
-        pub static CALL_COUNTER: Cell<u32> = Cell::new(0);
-    }
-    CALL_COUNTER.with(|c| {
-        c.set(c.get() + 1);
-        if c.get() > 1024 * 1024 * 10 {
-            let _ = pyo3::Python::acquire_gil();
-        }
-    });
-
-    let py = unsafe { pyo3::Python::assume_gil_acquired() };
+    // If we use `pyo3::Python::assume_gil_acquired()` then nothing ever
+    // empties the `register_owned` stack.
+    let gil = pyo3::Python::acquire_gil();
+    let py = gil.python();
 
     let mut slf = unsafe { py.from_borrowed_ptr::<pyo3::PyCell<CTracer>>(tracer) }.borrow_mut();
 
@@ -253,8 +289,8 @@ extern "C" fn trace_func(
         return 0;
     }
 
-    let py_frame = PyFrame(frame);
-    let result = match slf.handle_trace(py, event, &py_frame) {
+    let py_frame = unsafe { py.from_borrowed_ptr::<PyFrame>(frame.cast()) };
+    let result = match slf.handle_trace(py, event, py_frame) {
         Ok(()) => 0,
         Err(e) => {
             e.restore(py);
@@ -267,21 +303,21 @@ extern "C" fn trace_func(
 #[pyo3::prelude::pyclass]
 struct CFileDisposition {
     #[pyo3(get, set)]
-    original_filename: Option<pyo3::Py<pyo3::types::PyString>>,
+    original_filename: Option<pyo3::Py<PyString>>,
     #[pyo3(get, set)]
-    canonical_filename: Option<pyo3::Py<pyo3::types::PyString>>,
+    canonical_filename: Option<pyo3::Py<PyString>>,
     #[pyo3(get, set)]
-    source_filename: Option<pyo3::Py<pyo3::types::PyString>>,
+    source_filename: Option<pyo3::Py<PyString>>,
     #[pyo3(get, set)]
     trace: bool,
     #[pyo3(get, set)]
-    reason: Option<pyo3::Py<pyo3::types::PyString>>,
+    reason: Option<pyo3::Py<PyString>>,
     #[pyo3(get, set)]
     file_tracer: Option<pyo3::Py<pyo3::PyAny>>,
     #[pyo3(get, set)]
     has_dynamic_filename: bool,
 
-    file_data: HashSet<i32>,
+    file_data: HashSet<i64>,
 }
 
 #[pyo3::prelude::pymethods]
@@ -303,7 +339,7 @@ impl CFileDisposition {
 }
 
 #[pyo3::prelude::pymodule]
-fn tracer(_py: pyo3::Python<'_>, m: &pyo3::types::PyModule) -> pyo3::PyResult<()> {
+fn tracer(_py: pyo3::Python<'_>, m: &PyModule) -> pyo3::PyResult<()> {
     m.add_class::<CTracer>()?;
     m.add_class::<CFileDisposition>()?;
 
